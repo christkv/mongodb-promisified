4db6079e680823833f3ad840b961406c32586ca2
Christian Kvalheim <christkv@gmail.com>
2015-04-17 16:16:39 +0200

    Added bluebird library test

--- test/functional/mongoclient_tests.js
+++ test/functional/mongoclient_tests.js
@@ -20,3 +20,23 @@ exports['Should correctly execute insertOne'] = {
     });
   }
 }
+
+exports['Should correctly execute insertOne using Bluebird'] = {
+  metadata: { requires: { } },
+
+  // The actual test we wish to run
+  test: function(configuration, test) {
+    var Promise = require("bluebird")
+      , MongoClient = require('../..')(Promise).MongoClient;
+
+    co(function*(){
+      var client = yield MongoClient.connect('mongodb://localhost:27017/test');
+      // Close the connection
+      yield client.close();
+      test.done();
+    }).catch(function(err) {
+      console.log(err.stack)
+      test.ok(false);
+    });
+  }
+}
